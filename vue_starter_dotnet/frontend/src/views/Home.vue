c01196b01018472f497c05171f1bc05966749e67
Gene Huh <gshuh.mail@gmail.com>
2020-04-14 21:10:04 -0400

    modified home page with nav bar

--- vue_starter_dotnet/frontend/src/views/Home.vue
+++ vue_starter_dotnet/frontend/src/views/Home.vue
@@ -2,9 +2,6 @@
   <div class="home">
     <div class="content">
       <navbar/>
-      <router-link :to="{name: 'products-list'}" exact class="products" tag="button">Products List</router-link>
-      <router-link :to="{name: 'edit'}" class="edit" tag="button">Edit</router-link>
-      <router-view />
     </div>
   </div>
 </template>
