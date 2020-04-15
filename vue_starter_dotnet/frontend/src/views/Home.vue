5e2b3fd5ed9ed5872882b24a062a034a4fd15e1b
Shabnam Ahmed <shabnamrahmed@gmail.com>
2020-04-14 21:04:32 -0400

    Added user management options

--- vue_starter_dotnet/frontend/src/views/Home.vue
+++ vue_starter_dotnet/frontend/src/views/Home.vue
@@ -1,9 +1,10 @@
 <template>
   <div class="home">
     <header>
-    
+    <h2>Welcome</h2>
           <router-link :to="{name: 'products-list'}" exact class="products" tag="button">Products List</router-link>
           <router-link :to="{name: 'edit'}" class="edit" tag="button">Edit</router-link>
+          <router-link :to="{name: 'user-management'}" class="user-management" tag="button">User Management</router-link>
           
     </header>
     <div class="content">
--- vue_starter_dotnet/frontend/src/views/Login.vue
+++ vue_starter_dotnet/frontend/src/views/Login.vue
@@ -1,32 +1,69 @@
 <template>
-<div class="login">
+  <div class="login">
     <h1>Login</h1>
     <form>
-        <div class="form-element">
+      <div class="form-element">
         <label for="username">Username:</label>
-        <input id="username" type="text"></div>
+        <input id="username" type="text" required />
+      </div>
 
-        <div class="form-element">
+      <div class="form-element">
         <label for="password">Password:</label>
-        <input id="password" type="text"></div>
+        <input id="password" type="text" required />
+      </div>
     </form>
-    <router-link :to="{name: 'home'}"><button>Sign In</button></router-link>
-    
-</div>
+    <router-link :to="{name: 'home'}">
+      <button>Sign In</button>
+    </router-link>    
+  </div>
 </template>
 
 <script>
-export default {
-    name: 'login',
-    title: "Login",
-    data () {
-        return {
+import auth from "../auth";
 
-        }
+export default {
+  name: "login",
+  title: "Login",
+  data() {
+    return {
+      user: {
+        username: "",
+        password: ""
+      },
+      invalidCredentials: false
+    };
+  },
+  methods: {
+    login() {
+      fetch(url, {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(this.user)
+      })
+        .then(response => {
+          if (response.ok) {
+            return response.text();
+          } else {
+            this.invalidCredentials = true;
+          }
+        })
+        .then(token => {
+          if (token != undefined) {
+            if (token.includes('"')) {
+              token = token.replace(/"/g, "");
+            }
+            auth.saveToken(token);
+            this.$router.push("/");
+          }
+        })
+        .catch(err => console.error(err));
     }
-}
+  }
+};
 </script>
 
 <style scoped>
-
 </style>
