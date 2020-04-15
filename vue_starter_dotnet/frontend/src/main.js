6bc48fa23b41424b9d0ba0124ad4e8af7fe287fd
Gene Huh <gshuh.mail@gmail.com>
2020-04-14 22:04:37 -0400

    Making shiny main and app

--- vue_starter_dotnet/frontend/src/main.js
+++ vue_starter_dotnet/frontend/src/main.js
@@ -2,6 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import {library} from "@fortawesome/fontawesome-svg-core";
+import {faBars, faEdit, faTrash, faUser, faSave, faFileExport, faMinusCircle, faPlusCircle, faBan, faEye} from "@fortawesome/free-solid-svg-icons";
+library.add(faBars, faEdit, faTrash, faUser, faSave, faFileExport, faMinusCircle, faPlusCircle, faBan, faEye);
 Vue.config.productionTip = false
 
 new Vue({
