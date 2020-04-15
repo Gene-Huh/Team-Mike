b0cca1b3bb9b8c1d4a1973d7e345176b4105b2a5
Paul Curtin <curtin.paul.1@gmail.com>
2020-04-15 11:10:34 -0400

    Added Add and delete user tests. Updated delete user SQL statement

--- vue_starter_dotnet/backend/SampleApi.Tests/Tests/UserTests.cs
+++ vue_starter_dotnet/backend/SampleApi.Tests/Tests/UserTests.cs
@@ -24,31 +24,74 @@ namespace ProductApprovalTests.Tests
         [TestMethod]
         public void AddUserTest()
         {
+            int originalRowCount = GetRowCount("users");
+
             UsersSqlDAO dao = new UsersSqlDAO(connectionString);
-            IList<User> allUsers = dao.GetAllUsers();
-            int testResult = allUsers.Count;
-            int expectedResult = GetRowCount("users");
-            Assert.AreEqual(expectedResult, testResult);
+
+            User testUser = new User();
+            testUser.FirstName = "John";
+            testUser.LastName = "Smith";
+            testUser.Username = "johnsmith";
+            testUser.Role = "User";
+            testUser.Password = "password";
+            testUser.Salt = "salt";
+
+            dao.AddUser(testUser);
+
+            int expectedRowCount = GetRowCount("users");
+
+            Assert.AreEqual(originalRowCount + 1, expectedRowCount);
         }
 
         [TestMethod]
         public void UpdateUserTest()
         {
             UsersSqlDAO dao = new UsersSqlDAO(connectionString);
-            IList<User> allUsers = dao.GetAllUsers();
-            int testResult = allUsers.Count;
-            int expectedResult = GetRowCount("users");
-            Assert.AreEqual(expectedResult, testResult);
+            string updatedFirstName = "Jane";
+            string updatedLastName = "Doe";
+            string updatedRole = "Admin";
+
+            User testUser = new User();
+            testUser.FirstName = "John";
+            testUser.LastName = "Smith";
+            testUser.Username = "johnsmith";
+            testUser.Role = "User";
+            testUser.Password = "password";
+            testUser.Salt = "salt";
+
+
+            User updatedTestUser = new User();
+            testUser.FirstName = "Jane";
+            testUser.LastName = "Smith";
+            testUser.Username = "johnsmith";
+            testUser.Role = "User";
+            testUser.Password = "password";
+            testUser.Salt = "salt";
+
         }
 
         [TestMethod]
         public void DeleteUserTest()
         {
-            UsersSqlDAO dao = new UsersSqlDAO(connectionString);
-            IList<User> allUsers = dao.GetAllUsers();
-            int testResult = allUsers.Count;
-            int expectedResult = GetRowCount("users");
-            Assert.AreEqual(expectedResult, testResult);
+             UsersSqlDAO dao = new UsersSqlDAO(connectionString);
+
+            User testUser = new User();
+            testUser.FirstName = "John";
+            testUser.LastName = "Smith";
+            testUser.Username = "johnsmith";
+            testUser.Role = "User";
+            testUser.Password = "password";
+            testUser.Salt = "salt";
+
+            dao.AddUser(testUser);
+
+            int originalRowCount = GetRowCount("users");
+
+            dao.DeleteUser(testUser);
+
+            int expectedRowCount = GetRowCount("users");
+
+            Assert.AreEqual(originalRowCount - 1, expectedRowCount);
         }
 
     }
