54d3ec44423fd7a2323a9238329e3dd572173d23
Gene Huh <gshuh.mail@gmail.com>
2020-04-15 18:21:13 -0400

    readd password to user model

--- vue_starter_dotnet/backend/SampleApi/Models/User.cs
+++ vue_starter_dotnet/backend/SampleApi/Models/User.cs
@@ -10,6 +10,7 @@ namespace ProductApproval.Models
         public int UserId { get; set; }
         public string Role { get; set; }
         public string Username { get; set; }
+        public string Password { get; set; }
         public string LastName { get; set; }
         public string FirstName { get; set; }
     }
