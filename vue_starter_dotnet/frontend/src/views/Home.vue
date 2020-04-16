6bf57649113022e023099091dc7a79293a14b16c
Shabnam Ahmed <shabnamrahmed@gmail.com>
2020-04-16 11:16:57 -0400

    Added Team Mike info to homepage

--- vue_starter_dotnet/frontend/src/views/Home.vue
+++ vue_starter_dotnet/frontend/src/views/Home.vue
@@ -1,11 +1,29 @@
-<template>  
-    <div class="content">
-      <navbar/>       
-      </div>      
+<template>
+  <div class="content">
+    <navbar />
+    <div class="team-mike">
+      <div>
+        <h3>Searra Schell</h3>
+        <p>https://www.linkedin.com/in/searra-schell/</p>
+      </div>
+      <div>
+        <h3>Shabnam Ahmed</h3>
+        <p>https://www.linkedin.com/in/shabnam-ahmed/</p>
+      </div>
+      <div>
+        <h3>Gene Huh</h3>
+        <p>https://www.linkedin.com/in/geneshuh/</p>
+      </div>
+      <div>
+        <h3>Paul Curtin</h3>
+        <p>https://www.linkedin.com/in/curtinpaul1/</p>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import Navbar from "@/components/Navbar"
+import Navbar from "@/components/Navbar";
 import auth from "../auth";
 
 export default {
@@ -15,11 +33,25 @@ export default {
   data() {
     return {
       userRole: ""
-    }
+    };
   },
   created() {
     auth.getToken();
-    
   }
 };
 </script>
+<style>
+.team-mike {
+  display: flex;
+  justify-content: space-evenly;
+  flex-wrap: wrap;
+  height: 50vh;
+  align-items: center;
+}
+.team-mike div {
+  flex-basis: 50%;
+}
+h3 {
+  color: #f0ab00
+}
+</style>
